667cb4b2603d6f6ec7e49de36957b7c34f7ab3b3
Iustin55 <iustin.lozba@gmail.com>
2023-01-28 12:17:43 +0200

    Hello triangle ex2 - 2 VAO & VBO
    Draw two triangles using two sets of VAO and Vbos

--- GraphicsSandbox/GraphicsSandbox/Source.cpp
+++ GraphicsSandbox/GraphicsSandbox/Source.cpp
@@ -7,10 +7,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // Vertex data
-float vertices[] = {
+float vertices_1[] = {
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+     0.0f,  0.5f, 0.0f,
+};
+float vertices_2[] = {
+     0.5f, -0.5f, 0.0f,
+     1.0f,  0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
 };
 
 // Shader data
@@ -97,14 +102,27 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    //VAO and VBO
-    unsigned int VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    //VAO and VBO for the first triangle
+    unsigned int VAO1, VBO1;
+    glGenVertexArrays(1, &VAO1);
+    glGenBuffers(1, &VBO1);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindVertexArray(VAO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
+
+    // Vertex Attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //VAO and VBO for the second triangle
+    unsigned int VAO2, VBO2;
+    glGenVertexArrays(1, &VAO2);
+    glGenBuffers(1, &VBO2);
+
+    glBindVertexArray(VAO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_2), vertices_2, GL_STATIC_DRAW);
 
     // Vertex Attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -124,8 +142,14 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Render first triangle
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_LINE_LOOP, 0, 3);
+
+        // Render second triangle
+        glBindVertexArray(VAO2);
+        glDrawArrays(GL_LINE_LOOP, 0, 3);
 
         //Check call events
         glfwSwapBuffers(window);
@@ -134,8 +158,10 @@ int main()
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO1);
+    glDeleteVertexArrays(1, &VAO2);
+    glDeleteBuffers(1, &VBO1);
+    glDeleteBuffers(1, &VBO2);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
